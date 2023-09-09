bcba6553b5237f402ac330ac86b5513fb7904662
thatptr <chryripple@gmail.com>
2023-09-09 10:30:45 +0200

    Adding Price calculation

--- src/include/stock.py
+++ src/include/stock.py
@@ -1,13 +1,29 @@
+import account
+
 class market:
     def __init__(self, name: str, capital: int) -> None:
         self.name = name
         self.capital = capital
         self.stock = {
-            str(self.stock): 0,
+            str(self.stock): float(0),
         }
 
     def add_stock(self, stock_name: str, shares: int) -> None:
         self.stock[stock_name] = shares
 
-    def get_stock_price(self, stock) -> int:
+    def get_stock_price(self, stock) -> float:
         return self.stock[stock]
+
+    def calculate_stock_price(self, stock: str, amount: int) -> float:
+        if not stock in self.stock:
+            return False
+
+        new_stock_price = round(amount/self.get_stock_price(stock), 2)
+        
+        return new_stock_price
+
+    def buy_stock(self, stock: str, account: account.account, amount: int) -> bool:
+        x = self.calculate_stock_price(stock, amount)
+        self.stock[stock] = x
+        account.withdraw(int(x))
+        return True
--- src/test.py
+++ src/test.py
@@ -1,8 +1,10 @@
 from include import stock
+from include import account
 
 def main():
+  # b = account.account("yes", 1000000000)
   a = stock.market("Yaboi LLC", 10000)
-  a.add_stock("Yaboify", 2)
+  a.add_stock("Yaboify", 1000)
+  a.buy_stock("Yaboify", b, 2)
 
-  print(a.get_stock_price("Yaboify"))
   
